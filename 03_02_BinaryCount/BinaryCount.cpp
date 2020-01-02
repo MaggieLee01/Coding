577a5d8776cea326db74d385f6e0af402e4920d2
MaggieLee01 <1136053360@qq.com>
2020-01-02 12:19:07 +0800

    Revert "binary no success"
    This reverts commit 169da0820da65df26d2b446372f81dd4936de38e.

--- 03_02_BinaryCount/BinaryCount.cpp
+++ 03_02_BinaryCount/BinaryCount.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
-//自己编写难点：此处统计时端点处是否带等号，不好处理；如2,1,3,1,4,和数组2,1,3,3，等号的情况不一样
+
 int count(int numbers[], int length,int start, int end)
 {
 	int m = 0;
 	for (int i = 0; i < length; i++)
 	{
-		if (numbers[i] >= start && numbers[i] <= end)
+		if (numbers[i] > start && numbers[i] < end)
 			m++;
 	}
 	return m;
 }
-//写成递归式二分法的错误：最内层递归正确返回之后，进入了倒数第二层的函数其他部分
 bool duplicate(int numbers[], int length,int start,int end, int* duplication)
 {
 	if (numbers == nullptr || length <= 0)
@@ -24,16 +23,13 @@ bool duplicate(int numbers[], int length,int start,int end, int* duplication)
 		*duplication = start;
 		return true;
 	}
-	int mid = (end - start) >> 1;
-	 mid += start;
-
+	int mid = (start + end) >> 1;
+	
 	int no = count(numbers, length, start, mid);
-	if (no > mid-start)
+	if (no > mid)
 		duplicate(numbers, length, start, mid, duplication);
 	else
 		duplicate(numbers, length, mid, end, duplication);
-
-	return false;
 	
 }
 
