a95debb633c8f91c7923c9c57f54db9b7dd6ba2a
MaggieLee01 <1136053360@qq.com>
2020-02-11 16:00:45 +0800

    DP框架进行中，回文子串、回文子序列

---
+++ DP05_LongestPalindrome/LongestPalindrome.cpp
@@ -0,0 +1,116 @@
+//求最长回文子串。回文串就是正着读和反着读都一样的字符串
+//子串一定是连续的，而子序列不一定是连续的
+//2019-10-03
+//未在系统上测试，测试代码没问题，基本按照文章思路
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+//dict[i]表示以i结束的回文子串的长度
+//char* LongestPalindrome(const char* str)
+//{
+//	if (str == nullptr)
+//		return nullptr;
+//	int strLength = strlen(str);
+//	std::vector<int> dict(strLength + 1, 1);
+//	if (str[0] == str[1]) dict[1] = 2;
+//	for (int i = 2; i < strLength; i++)
+//	{
+//		if (str[i] == str[i - 1])
+//			dict[i] = 2;
+//		/*if (str[i] == str[i - 1])
+//			dict[i] = dict[i - 1] + 1;*/
+//		if (str[i] == str[i - 2])
+//			dict[i] = dict[i - 2] + 2;		
+//		if (dict[i - 1] != 1)
+//		{
+//			if (str[i] == str[i - 1 - dict[i - 1] ] )
+//				dict[i] = dict[i - 1] + 2;
+//		}
+//	}
+//	return nullptr;
+//}
+
+//尝试失败，想一次遍历，通过n维数组记录回文子串的长度，奈何，回文子串需要遍历全部之后才能得出结果
+//这种情况需要马拉车算法
+//上面的代码，仅仅能得出对称轴不变换的回文子串的长度，对于对称轴不断变换的子串，无能为力
+//看完答案，仍然需要两次遍历，时间复杂度为n*n
+
+//开始使用std::string标准库函数
+using std::vector;
+using std::string;
+
+//返回以m n为中心的回文字符串
+//string Palindrome(string str,int m,int n)
+//{
+//	int Length = str.size();
+//	int i = m, j = n;
+//	while (i >= 0 && j <= Length && str[i] == str[j])
+//	{
+//		i--; 
+//		j++;
+//	}
+//	string substr = str.substr(i + 1, j - 1 - i);
+//	return substr;
+//}
+
+//看完答案，简化代码
+string Palindrome(string str, int i, int j)
+{
+	int Length = str.size();
+	while (i >= 0 && j <= Length && str[i] == str[j])
+	{
+		i--; 
+		j++;
+	}
+	return str.substr(i + 1, j - 1 - i);
+}
+//string LongestPalindrome(string str)
+//{
+//	int Length = str.size();
+//	vector<int> dict(Length, 0);
+//	
+//	for (int i = 0; i < Length; i++)
+//	{
+//		string str1 = Palindrome(str, i, i);
+//		string str2 = Palindrome(str, i, i + 1);
+//		dict[i] = str2.size() > str1.size() ? str2.size() : str1.size();		
+//	}
+//	auto max = max_element(dict.begin(), dict.end()) - dict.begin();//头文件#include<algorithm>
+//	string str1 = Palindrome(str, max, max);
+//	string str2 = Palindrome(str, max, max+1);
+//	return str1.size() > str2.size() ? str1 : str2;
+//}
+
+//看完答案，简化代码
+//无需向量存储各个字符串回文子串的长度，只需一个空间不断更新最大值即可
+string LongestPalindrome(string str)
+{
+	int Length = str.size();
+	string max;
+
+	for (int i = 0; i < Length; i++)
+	{
+		string str1 = Palindrome(str, i, i);
+		string str2 = Palindrome(str, i, i + 1);
+		max = max.size() > str1.size() ? max : str1;
+		max = max.size() > str2.size() ? max : str2;
+	}
+	return max;
+}
+
+int main(void)
+{
+	const char* str = "abcdcbeabccba";
+	std::cout << LongestPalindrome(str) << std::endl;//加头文件<string>才能打印
+	const char* str1 = "aaaaaaaa";
+	std::cout << LongestPalindrome(str1) << std::endl;
+	const char* str2 = "abcbccbcba";
+	std::cout << LongestPalindrome(str2) << std::endl;
+	const char* str3 = "baaaaaaaa";
+	std::cout << LongestPalindrome(str3) << std::endl;
+	const char* str4 = "aadeccbccedddddddddd";
+	std::cout << LongestPalindrome(str4) << std::endl;	
+}
---
+++ DP06_LongestPalindromeSubseq/LongestPalindromeSubseq.cpp
@@ -0,0 +1,59 @@
+//求最长回文子序列的长度。
+//子串一定是连续的，而子序列不一定是连续的
+//2019-11-13
+//未在系统上测试，测试代码没问题，基本按照文章思路
+
+//三步法:
+//明确dp[i][j]表示的是什么，ij之间的长度 还是 ij为起始位置？
+//		例如：bbbbab dp[0][4]应该为多少？目前假设为4 
+//		暂定长度方便找出相互之间的关系
+//起始位置
+//转移方程
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using std::vector;
+using std::max;
+
+int LengthOfLPS(const char* str)
+{
+	if (str == nullptr) return 0;
+	int length = strlen(str);
+	vector< vector<int> > dict(length + 1, vector<int>(length + 1, 0));
+	//困难点：计算第i行的时候，i+1行还没有计算出来 咋整？
+	//		画出二维矩阵 更好看清遍历方向
+	//		转移方程目前也有难点，对于重复数字【起始值为1】、非重复数字情况【起始值为0】不太一样	//此坑为自己理解回文序列长度有误。
+	//画出二维矩阵，思考后的遍历顺序基本没问题，和答案一样
+	for (int i = length - 1; i >= 0; i--)   
+	{
+		dict[i][i] = 1;
+		for (int j = i + 1; j < length; j++)
+		{			
+			if (str[i] == str[j])
+				dict[i][j] = dict[i + 1][j - 1] + 2;
+						
+			else			
+				dict[i][j] = max(dict[i + 1][j], dict[i][j - 1]);			
+		}
+	}
+	return dict[0][length - 1];
+}
+
+int main(void)
+{
+	const char* str1 = "babbbb";
+ 	std::cout << LengthOfLPS(str1) << std::endl;
+
+	const char* str2 = "abcdecnambl";//注意beb的回文长度为3，不是2，掉在此坑太久了
+	std::cout << LengthOfLPS(str2) << std::endl;
+
+	const char* str3 = "abcdecnbmal";
+	std::cout << LengthOfLPS(str3) << std::endl;
+
+	const char* str4 = "bababaccababab";
+	std::cout << LengthOfLPS(str4) << std::endl;
+
+	const char* str5 = "baba";
+	std::cout << LengthOfLPS(str5) << std::endl;
+}
